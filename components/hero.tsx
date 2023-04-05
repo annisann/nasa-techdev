781e10f381038c16421ada6c9410df22b1e9339d
annisann <hello.annisann@gmail.com>
2023-04-05 16:21:07 +0700

    init hero component and its css style

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,17 +1,22 @@
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/hero.module.css'
+import Link from 'next/link'
 
 export default function Hero() {
     const heroParagraph = `
 	Technology development funded by NASA to explore space, understand the universe, and improve aeronautics
 	`
     return (
-        <div className={styles.hero}>
+        <div className={styles.container}>
             <div className={styles.text}>
-                <p className="title"> NASA TechDev </p>
-                <p className="paragraph"> {heroParagraph} </p>
-                <button> Explore </button>
+                <p className={styles.title}> NASA TechDev </p>
+                <p className={styles.paragraph}> {heroParagraph} </p>
+                <button>
+                    <Link href="/projects">
+                        Explore
+                    </Link>
+                </button>
             </div>
-            <div> I M A G E </div>
+            <div> I M A G EEEEEEEEEEEEEEEE EEEEEEE EEEEEEEE EEEEEEEEEEEEEEEEEEEEEEEEEEEEE </div>
         </div>
     )
 }
