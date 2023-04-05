80e08dfe88494b489a0239e5e511929789e38ef5
annisann <hello.annisann@gmail.com>
2023-04-05 19:51:29 +0700

    still cooking...

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,5 @@
-import { ENDPOINT } from "@/lib/consts";
-import axios from "axios"
+import { getEndpoint } from "@/lib/helper";
+import { Table } from "antd";
 import { useEffect, useState } from "react"
 
 interface Project {
@@ -13,10 +13,26 @@ interface ApiResponse {
 };
 
 interface ProjectDetail {
-
+    projectId: number;
+    title: string;
+    description: string;
+    benefits: string;
+    statusDescription: number;
+    // "startYear": 2014,
+    // "startMonth": 4,
+    // "endYear": 2017,
+    // "endMonth": 6,
+    // program: object;
+    // leadOrganization: object;
+    // supportingOrganizations: object;
+    lastUpdated: string;
+    startDateString: string;
+    endDateString: string;
 }
 
 export default function Projects() {
+    const [isLoading, setIsLoading] = useState(true)
+
     // FETCH DATA
     const [projectsData, setProjectsData] = useState<ApiResponse>({
         projects: [],
@@ -24,12 +40,30 @@ export default function Projects() {
     });
 
     const fetchProjects = async () => {
-        const response = await fetch(ENDPOINT);
-        const data = await response.json();
-        setProjectsData(data);
+        await fetch(getEndpoint())
+            .then(async (response) => await response.json())
+            .then((data: ApiResponse) => {
+                console.log('data', data)
+                setProjectsData(data);
+                // data.projects?.forEach(
+                //     async (project) => {
+                //         await fetch(getEndpoint(project.projectId))
+                //         .then(async (response) => response.json())
+                //         .then((detail: ProjectDetail) => {
+                //             projectsData.projects.push(detail)
+                //             // setProjectsData({
+                //             //     ...projectsData,
+                //             //     projects: detail
+                //             // })
+                //         })
+                //     }
+                // )
+                setIsLoading(false)
+            });
     }
 
     useEffect(() => {
+        setIsLoading(true)
         fetchProjects()
     }, [])
 
@@ -37,16 +71,23 @@ export default function Projects() {
 
     return (
         <div>
-            <h1>Projects</h1>
+            <p>Projects</p>
             <p> Total Projects: {projectsData.totalCount} </p>
             <ul>
-                {projectsData.projects.map(project => (
+                {isLoading ? <li> Loading... </li> : projectsData.projects?.map(project => (
                     <li key={project.projectId}>
-                        <h3>{project.projectId}</h3>
-                        <p>{project.lastUpdated}</p>
+                        <p> Project ID: {project.projectId}</p>
+                        <p> Last Updated: {project.lastUpdated}</p>
                     </li>
                 ))}
             </ul>
+            <Table
+                // columns={columns}
+                dataSource={projectsData.projects}
+                bordered
+                size="middle"
+                scroll={{ x: 'calc(700px + 50%)', y: 240 }}
+            />
         </div>
     )
 }
