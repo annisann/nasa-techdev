8db55f1aaf76a23de1173a08b296c2b0dcfa47b7
annisann <hello.annisann@gmail.com>
2023-04-05 17:03:50 +0700

    update global title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title> NASA TechDev </title>
+        <meta name="description" content="Made with NASA TechPort API" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
