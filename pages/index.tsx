8ec45a125d706205fb335efa064d0e0db058f9d2
annisann <hello.annisann@gmail.com>
2023-04-05 16:22:03 +0700

    update import: latestProject -> showCase

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Hero from '@/components/hero'
-import Showcase from '@/components/latestProject'
+import Showcase from '@/components/showcase'
 
 const inter = Inter({ subsets: ['latin'] })
 
