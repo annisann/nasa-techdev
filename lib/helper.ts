3f96074f2620d428adb28fb7dc8992b8951601d9
annisann <hello.annisann@gmail.com>
2023-04-07 12:17:10 +0700

    🔒 store secrets to env

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,6 +1,8 @@
-import { API_KEY } from "./consts"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 export function getEndpoint(projectId?: number) {
     const idParam: string = projectId? `/${projectId}` : ""
-    return `https://api.nasa.gov/techport/api/projects${idParam}?api_key=${API_KEY}`
+    return `https://api.nasa.gov/techport/api/projects${idParam}?api_key=${process.env.API_KEY}`
 }
