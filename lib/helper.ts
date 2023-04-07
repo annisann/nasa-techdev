25b0d724866be8a77889e4538eecd3ec73cf7e43
annisann <hello.annisann@gmail.com>
2023-04-07 13:03:19 +0700

    remove dotenv

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,7 +1,3 @@
-import dotenv from "dotenv"
-
-dotenv.config()
-
 export function getEndpoint(projectId?: number) {
     const idParam: string = projectId? `/${projectId}` : ""
     return `https://api.nasa.gov/techport/api/projects${idParam}?api_key=${process.env.API_KEY}`
